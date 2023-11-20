942c26c9488899c26cd375ae6c2f3c12846ca6f1
CarlaDBlue <chidaro@edu.upv.es>
2023-11-20 23:57:44 +0100

    Guardamos el fichero DyV.h

---
+++ DyV.h
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> a;
+vector<float> b(10);
+vector<short> d{1, 2, 3};
+
+size_t val = b.size();
+a.push_back(10);
+d.pop_back();
+float val2 = b[3];
